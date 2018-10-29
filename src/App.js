9624cbef554f91e9e52e3b0fc8d63b170808ce09
Sanja <codesanja@gmail.com>
2018-10-29 22:07:35 +0000

    Adding router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 import Dashboard from './components/Dashboard';
+import Lost from './components/Lost';
 
 class App extends Component {
   render() {
     return (
-        <Dashboard />
+      <Router>
+          <Switch>
+              <Route exact path="/" render={(props) => {
+                  return <Home {...props} />
+              }} />
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route component={Lost} />
+          </Switch>
+      </Router>
     );
+
   }
 }
 
---
+++ src/components/Lost.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Lost extends Component {
+    render() {
+        return (
+            <div>Lost page..</div>
+        );
+    }
+}
+
+
+export default Lost;

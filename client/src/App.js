044fe270fcd8989f5914e46d16e0f674581906bb
Sanja <codesanja@gmail.com>
2018-11-07 18:32:12 +0000

    Registration works! :D

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 import Home from './components/content/Home';
 import Dashboard from './components/content/Dashboard';
 import Lost from './components/content/Lost';
-import Register from './components/content/Register';
+import RegistrationForm from './components/auth/RegistrationForm';
 import Login from './components/auth/Login';
 
 const baseUrl = 'https://dev-465791.oktapreview.com';
@@ -32,7 +32,7 @@ class App extends Component {
               </div>
               <Switch>
                   <Route exact path="/" component={Home} />
-                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/register' render={() => <RegistrationForm baseUrl={baseUrl} />}  />
                   <SecureRoute path="/dashboard" component={Dashboard} />
                   <Route path='/login' render={() => <Login baseUrl={baseUrl} />} />
                   <Route path='/implicit/callback' component={ImplicitCallback}/>

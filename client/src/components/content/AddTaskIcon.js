415e3a1fbc323bf534f1f4c62544d2785bab9209
Sanja <codesanja@gmail.com>
2018-12-10 17:29:41 +0000

    Popover work in progress.

--- client/src/components/content/AddTaskIcon.js
+++ client/src/components/content/AddTaskIcon.js
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import Popover from '@material-ui/core/Popover';
+import { withStyles } from '@material-ui/core/styles';
 
-function AddTaskIcon(props) {
-    return (
-        <Fab className="add-task-icon" color="action" size="small" aria-label="Add">
-            <AddIcon />
-        </Fab>
-    );
+const styles = theme => ({
+    popover: {
+        pointerEvents: 'none',
+    },
+    paper: {
+        padding: theme.spacing.unit,
+    },
+});
+
+class AddTaskIcon extends Component {
+    state = {
+        anchorEl: null,
+    };
+
+    handlePopoverOpen = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handlePopoverClose = () => {
+        this.setState({ anchorEl: null });
+    };
+
+    render() {
+        const { classes } = this.props;
+        const { anchorEl } = this.state;
+        const open = Boolean(anchorEl);
+
+        return (
+            <div>
+                <Fab
+                    aria-label="Add"
+                    aria-owns={open ? 'mouse-over-popover' : undefined}
+                    aria-haspopup="true"
+                    className="add-task-icon"
+                    color="default" size="small"
+                    onMouseEnter={this.handlePopoverOpen}
+                    onMouseLeave={this.handlePopoverClose}
+                >
+                    <AddIcon />
+                </Fab>
+                <Popover
+                    id="mouse-over-popover"
+                    className={classes.popover}
+                    classes={{
+                        paper: classes.paper,
+                    }}
+                    open={open}
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    onClose={this.handlePopoverClose}
+                    disableRestoreFocus
+                >
+                    <div>I use Popover.</div>
+                </Popover>
+            </div>
+        )
+    };
 }
 
-export default AddTaskIcon;
+AddTaskIcon.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AddTaskIcon);

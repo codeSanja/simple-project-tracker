25549525dc24526c07ec34a77d373d012706a46a
Sanja <codesanja@gmail.com>
2018-12-10 17:53:00 +0000

    Popover work in progress.

--- client/src/components/content/AddTaskIcon.js
+++ client/src/components/content/AddTaskIcon.js
@@ -4,6 +4,11 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import Popover from '@material-ui/core/Popover';
 import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
     popover: {
@@ -12,6 +17,23 @@ const styles = theme => ({
     paper: {
         padding: theme.spacing.unit,
     },
+    card: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+    italic: {
+        fontStyle: 'italic'
+    }
 });
 
 class AddTaskIcon extends Component {
@@ -31,6 +53,7 @@ class AddTaskIcon extends Component {
         const { classes } = this.props;
         const { anchorEl } = this.state;
         const open = Boolean(anchorEl);
+        const bull = <span className={classes.bullet}>•</span>;
 
         return (
             <div>
@@ -39,7 +62,8 @@ class AddTaskIcon extends Component {
                     aria-owns={open ? 'mouse-over-popover' : undefined}
                     aria-haspopup="true"
                     className="add-task-icon"
-                    color="default" size="small"
+                    color="default"
+                    size="small"
                     onMouseEnter={this.handlePopoverOpen}
                     onMouseLeave={this.handlePopoverClose}
                 >
@@ -54,17 +78,29 @@ class AddTaskIcon extends Component {
                     open={open}
                     anchorEl={anchorEl}
                     anchorOrigin={{
-                        vertical: 'bottom',
-                        horizontal: 'left',
+                        vertical: 'top',
+                        horizontal: 'center',
                     }}
                     transformOrigin={{
                         vertical: 'top',
-                        horizontal: 'left',
+                        horizontal: 'center',
                     }}
                     onClose={this.handlePopoverClose}
                     disableRestoreFocus
                 >
-                    <div>I use Popover.</div>
+                    <Card className={classes.card}>
+                        <CardContent>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                Material UI
+                            </Typography>
+                            <Typography className={classes.italic} variant="h5" component="h2">
+                                add new task
+                            </Typography>
+                            <Typography className={classes.pos} color="textSecondary">
+                                feature is in progress.
+                            </Typography>
+                        </CardContent>
+                    </Card>
                 </Popover>
             </div>
         )

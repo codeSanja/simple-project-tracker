6b9da036948e597017bd01ab084d940a81363bbb
Sanja <codesanja@gmail.com>
2019-01-09 15:12:40 +0000

    Tests - work in progress.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 import App from './App';
+// import OktaSignInWidget from './components/auth/OktaSignInWidget';
+import InProgressCard from './components/content/InProgressCard';
+import Counter from './components/content/Counter';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+HTMLCanvasElement.prototype.getContext = () => {
+    return {
+        backingStorePixelRatio: 0
+    }
+};
+
+const setup = (props={}, state=null) => {
+    const wrapper = shallow(<App {...props} />)
+    if (state) wrapper.setState(state)
+    return wrapper
+}
+
+it('shallow renders <Counter /> without crashing', () => {
+    const wrapper = shallow(<Counter />)
+    expect(wrapper).toBeTruthy();
 });
+
+
+it('renders <InProgressCard /> without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<InProgressCard />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+test('counter starts at 0', () => {
+    const wrapper = setup();
+    const initialCounterState = wrapper.setState('counter');
+    expect(initialCounterState).toBe(0)
+})
--- client/src/components/content/Lost.js
+++ client/src/components/content/Lost.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from "react-router-dom";
-
+import Counter from './Counter'
 class Lost extends Component {
     render() {
         return (
+        <Fragment>
             <div>Lost page.. <Link to="/">Go Home</Link></div>
+            <Counter />
+        </Fragment>
         );
     }
 }
